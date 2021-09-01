2e591f8e06f6fb23635026fb261814f5ea4053ba
Guilherme Haynes Howe <zerossb@gmail.com>
2021-09-01 12:13:07 -0300

    Add Functions to FunctionField

--- src/Fields/FunctionField.php
+++ src/Fields/FunctionField.php
@@ -4,8 +4,21 @@ namespace Bildvitta\Navpi\Fields;
 
 class FunctionField extends Field
 {
+    protected array $functionParams = [];
+
     public function __construct($name)
     {
         parent::__construct($name, 'function');
+    }
+
+    public function setParameter($key, $value)
+    {
+        $this->functionParams[$key] = $value;
+        return $this;
+    }
+
+    public function getFunctionParams()
+    {
+        return $this->functionParams;
     }
 }
