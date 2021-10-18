397ef0c949415a9121d134b4b203b52c43e8a28f
BILD\jean.garcia <jean.garcia@bild.com.br>
2021-10-18 15:57:30 -0300

    implements functions fields in attributes

--- src/NavpiResource.php
+++ src/NavpiResource.php
@@ -96,7 +96,7 @@ abstract class NavpiResource extends JsonResource
             if (request()->has('attributes')) {
                 $attributes = request()->get('attributes');
 
-                if (is_array($attributes) && !in_array($name, $attributes)) {
+                if (is_array($attributes) && ! in_array($name, $attributes)) {
                     continue;
                 }
             } else {
@@ -146,6 +146,11 @@ abstract class NavpiResource extends JsonResource
                 if (request()->has('attributes')) {
                     $attributes = request()->get('attributes');
 
+                    if ($field->getType() == 'function') {
+                        $item[$name] = $resource->$name($field->getFunctionParams());
+                        continue;
+                    }
+
                     if (is_array($attributes) && in_array($name, $attributes)) {
                         $item[$name] = $resource->$name;
 
@@ -190,7 +195,7 @@ abstract class NavpiResource extends JsonResource
                     }
 
                     if ($field->getType() == 'date') {
-                        if (!is_null($resource->$name)) {
+                        if (! is_null($resource->$name)) {
                             $item[$name] = Carbon::parse($resource->$name)->format('Y-m-d');
                         } else {
                             $item[$name] = $resource->$name;
