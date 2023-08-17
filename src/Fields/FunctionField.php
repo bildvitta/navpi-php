277b836d6f89edb11c7b516ee9f23402aa3b6e1b
Daniel Bueno <dkbueno@gmail.com>
2023-08-17 12:13:49 -0300

    including options on funcion field

--- src/Fields/FunctionField.php
+++ src/Fields/FunctionField.php
@@ -8,4 +8,31 @@ class FunctionField extends Field
     {
         parent::__construct($name, 'function');
     }
+
+    public function setParameter($key, $value)
+    {
+        $this->functionParams[$key] = $value;
+        return $this;
+    }
+
+    public function getFunctionParams()
+    {
+        return $this->functionParams;
+    }
+
+    public function options($value, $parsed = false)
+    {
+        if ($parsed) {
+            return $this->addParameter('options', $value);
+        }
+
+        $options = [];
+        foreach ($value as $key => $label) {
+            array_push($options, [
+                'label' => $label,
+                'value' => $key
+            ]);
+        }
+        return $this->addParameter('options', $options);
+    }
 }
